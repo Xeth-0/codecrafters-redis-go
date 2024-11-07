abd6b891b173397446a327164bb65ecdd153d789
Xeth-0 <tediberhanu777@gmail.com>
2024-11-07 10:03:18 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,63 +4,45 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
-
-	// 10 just because. Wanted to avoid the constant array resize/reallocations at low numbers.
-	queue := make([]net.Conn, 10)
+	defer listener.Close()
 
 	for {
-		// Accept new connection
-		newConn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		defer newConn.Close()
-
-		queue = append(queue, newConn)
-
-		// My makeshift queue pop(dequeue)
-		conn := queue[0]
-		if len(queue) == 1 {
-			queue = make([]net.Conn, 10)
-		} else {
-			queue = queue[1:]
+			fmt.Println("Error accepting connection")
 		}
 
-		// Handle the connection
-		handleConnection(conn)
+		go handleConnection(conn)
 	}
 }
 
+
 func handleConnection(conn net.Conn) {
-	bytesRead := make([]byte, 128)
-	_, err := conn.Read(bytesRead)
-	if err != nil {
-		fmt.Println("Error reading bytes from connection: ", err.Error())
-		os.Exit(1)
+	// fmt.Println("Connection: ", conn)
+	defer conn.Close()
+
+	for {
+		bytesRead := make([]byte, 128)
+		conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
+		_, err := conn.Read(bytesRead)
+		if err != nil { // nothing read or encountered an error.
+			return
+		}
+	
+		// Debugging incoming stream.
+		fmt.Println("Incoming bytes =>", bytesRead)
+	
+		// Hardcoding the response for this stage (Parsing comes later).
+		conn.Write([]byte("+PONG\r\n"))
 	}
-
-	// Debugging incoming stream.
-	fmt.Println("Incoming bytes =>", bytesRead)
-
-	// Hardcoding the response for this stage (Parsing comes later).
-	conn.Write([]byte("+PONG\r\n"))
 }
--- go.mod
+++ go.mod
@@ -9,3 +9,11 @@
 module github.com/codecrafters-io/redis-starter-go
 
 go 1.22
+
+require (
+	github.com/gotcp/epoll v1.3.11 // indirect
+	github.com/wuyongjia/hashmap v1.0.6 // indirect
+	github.com/wuyongjia/pool v1.0.7 // indirect
+	github.com/wuyongjia/threadpool v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
+)
