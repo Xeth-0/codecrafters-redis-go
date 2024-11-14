468ee24f335cbdb4f13a1e05f1e6cb851acd776a
Xeth-0 <tediberhanu777@gmail.com>
2024-11-14 09:56:18 +0300

    codecrafters submit [skip ci]

--- app/respEncode.go
+++ app/respEncode.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 )
 
+// returns the resp bulk-string encoded value of the string provided.
 func respEncodeBulkString(str string) string {
 	return fmt.Sprintf("$%d\r\n%s\r\n", len(str), str)
 }
 
+// returns the resp simple-string encoded value of the string provided.
 func respEncodeString(str string) string {
 	return fmt.Sprintf("+%s\r\n", str)
 }
 
+// returns a resp-array encoded string, containing bulk-string encoded values
+// of the strings provided. (strs should contain un-encoded values).
 func respEncodeStringArray(strs []string) string {
 	arrayLength := len(strs)
-
 	arrayString := fmt.Sprintf("*%d\r\n", arrayLength)
 
 	for _, str := range strs {
--- app/server.go
+++ app/server.go
@@ -18,9 +18,9 @@ func main() {
 
 	// Get the directory and filename for the rdb store.
 	dirFlag := flag.String("dir", "dump", "rdb store directory")
+	replicaOfFlag := flag.String("replicaof", "master", "master or slave")
 	dbFileNameFlag := flag.String("dbfilename", "dump.rdb", "rdb store filename")
 	portFlag := flag.Int("port", 6379, "the port that this redis server will use to run")
-	replicaOfFlag := flag.String("replicaof", "master", "master or slave")
 
 	flag.Parse()
 
@@ -29,31 +29,30 @@ func main() {
 	port := *portFlag
 	replicaOf := *replicaOfFlag
 
+	CONFIG.port = port
+	CONFIG.rdbDbFileName = dbFileName
+	CONFIG.rdbDir = dir
+
 	fmt.Println(replicaOf)
 
-	if replicaOf != "master" { // is a replica
+	if replicaOf != "master" {
+		// is a replica.
 		CONFIG.isSlave = true
-
 		r := strings.Split(replicaOf, " ")
 		if len(r) < 2 {
 			fmt.Println("invalid replica flag. try agian")
 			os.Exit(0)
 		}
-
 		CONFIG.masterHost = r[0]
 		CONFIG.masterPort = r[1]
-		// call the replica thingy
 
+		// establish connection with the master
 		sendHandshake()
 	} else {
 		// set the replication ID and offset
 		CONFIG.masterReplID = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
 		CONFIG.masterReplOffset = 0
 	}
-
-	CONFIG.port = port
-	CONFIG.rdbDbFileName = dbFileName
-	CONFIG.rdbDir = dir
 
 	startServer()
 }
@@ -62,12 +61,49 @@ func sendHandshake() {
 	address := fmt.Sprintf("%s:%s", CONFIG.masterHost, CONFIG.masterPort)
 	m, err := net.Dial("tcp", address)
 	if err != nil {
-		fmt.Println("error sending handshake to master: ", err)
+		fmt.Println("error sending handshake to master: cannot establish dialup with the master: ", err)
 		os.Exit(0)
 	}
-	req := make([]string, 1)
-	req[0] = "PING"
-	m.Write([]byte(respEncodeStringArray(req)))
+
+	// Handshake 1/3 - Send the first PING command
+	pingReq := make([]string, 1)
+	pingReq[0] = "PING"
+	m.Write([]byte(respEncodeStringArray(pingReq)))
+
+	// Response from handshake 1
+	responseBuffer := make([]byte, 1024)
+	_, err = m.Read(responseBuffer)
+	if err != nil {
+		fmt.Println("error recieving handshake response from master: ", err)
+		os.Exit(0)
+	}
+
+	// going to assume the response is pong for now
+	response, err := parseRESP([]byte(responseBuffer))
+	if err != nil {
+		fmt.Println("error recieving handshake response from master: ", err)
+	}
+	fmt.Println(response.respData.String)
+	if response.respData.String != "PONG" {
+		fmt.Println("error recieving handshake response from master: wrong response(expected: +PONG\r\n)")
+		os.Exit(0)
+	}
+
+	// Handshake 2/3 - Send the first REPLCONF command
+	replconfReq := make([]string, 0, 3)
+	replconfReq = append(replconfReq, "REPLCONF")
+	replconfReq = append(replconfReq, "listening-port")
+	replconfReq = append(replconfReq, fmt.Sprintf("%d", CONFIG.port))
+
+	m.Write([]byte(respEncodeStringArray(replconfReq)))
+
+	// Handshake 3/3 - Send the second REPLCONF command
+	replconfReq2 := make([]string, 0, 3)
+	replconfReq2 = append(replconfReq2, "REPLCONF")
+	replconfReq2 = append(replconfReq2, "capa")
+	replconfReq2 = append(replconfReq2, "psync2")
+
+	m.Write([]byte(respEncodeStringArray(replconfReq2)))
 }
 
 // Setup the tcp listener on the port specified
@@ -79,11 +115,9 @@ func startServer() {
 	}
 	defer listener.Close()
 
-	// Setup the RDB
-	RDB = setupRDB(CONFIG.rdbDir, CONFIG.rdbDbFileName)
+	RDB = setupRDB(CONFIG.rdbDir, CONFIG.rdbDbFileName) // Setup the RDB
 
-	// Listen for connections.
-	for {
+	for { // Listen for connections.
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection")
@@ -105,7 +139,6 @@ func handleConnection(conn net.Conn) {
 		}
 
 		readBuffer = readBuffer[:n] // Cut off the empty bytes at the end
-
 		// fmt.Println("--Incoming bytes => ", readBuffer)
 
 		// parse the resp request into a struct for easier manipulation
